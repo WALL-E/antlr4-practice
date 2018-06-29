add9b76cefd74303ee1b1222bb2084d9650be1e8
zhangzheng <zzgigi2003@163.com>
2018-06-29 10:35:54 +0800

    json(java) 优化t.sh

--- json/java/t.sh
+++ json/java/t.sh
@@ -3,4 +3,4 @@
 export CLASSPATH=".:/usr/local/lib/antlr-4.7.1-complete.jar:$CLASSPATH"
 
 javac *.java
-java org.antlr.v4.gui.TestRig JSON json -tree -gui
+cat t.json | java org.antlr.v4.gui.TestRig JSON json -tree -gui
